699a2c56952b31ae7bc8efe3c5219ce16143f013
johnnycashash <jagansiiinghchoudhary@gmail.com>
2018-03-10 19:08:33 +0530

    Basic template without db

---
+++ src/main/java/com/hitchhike/bungee/BungeeApplication.java
@@ -0,0 +1,12 @@
+package com.hitchhike.bungee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BungeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BungeeApplication.class, args);
+	}
+}
